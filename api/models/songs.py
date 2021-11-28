8170ce2bf66b1dd1ee962f0cc088c7ac5a0ec6eb
Kevin Cook <kkeys99@yahoo.com>
2021-11-28 00:30:34 -0500

    Refactor song model and serializer

--- api/models/songs.py
+++ api/models/songs.py
@@ -1,3 +1,5 @@
+from typing import Dict, List, Optional
+
 from django.db import models
 
 
@@ -14,13 +16,63 @@ class Tag(models.Model):
     name = models.TextField()
     tagType = models.TextField(choices=TagTypes.choices)
 
-    @property
-    def tagTypeLabel(self) -> str:
-        return Tag.TagTypes(self.tagType).label
+
+def addTagProperties(namesAndTypes: Dict[str, Tag.TagTypes]):
+    def setProperties(cls):
+        for name, tagType in namesAndTypes.items():
+
+            def getter(self, tagType=tagType):
+                return self._groupTags(tagType)
+
+            def setter(self, newTags, tagType=tagType):
+                return self._updateTagGroup(newTags, tagType)
+
+            prop = property(getter, setter)
+            setattr(cls, name, prop)
+        return cls
+
+    return setProperties
 
 
+propertyNamesWithTypes = {
+    "sheets": Tag.TagTypes.SHEET,
+    "composers": Tag.TagTypes.COMPOSER,
+    "arrangers": Tag.TagTypes.ARRANGER,
+    "genres": Tag.TagTypes.GENRE,
+    "keys": Tag.TagTypes.KEY,
+    "time": Tag.TagTypes.TIME,
+    "tempos": Tag.TagTypes.TEMPO,
+}
+
+
+@addTagProperties(propertyNamesWithTypes)
 class Song(models.Model):
     name = models.TextField()
     dateAdded = models.DateTimeField(auto_now_add=True)
     note = models.TextField(null=True, blank=True)
     tags = models.ManyToManyField(Tag)
+
+    def _groupTags(self, tagType: Tag.TagTypes):
+        return (
+            self.tags.filter(tagType=tagType)
+            .order_by("name")
+            .values_list("name", flat=True)
+        )
+
+    def _updateTagGroup(self, newTagNames: Optional[List[str]], tagType: Tag.TagTypes):
+        oldTags = self.tags.filter(tagType=tagType)
+
+        # clear the old tags from the relationship
+        for tag in oldTags:
+            self.tags.remove(tag)
+
+        # create a new tag if needed, and add all the new tags to the relationship
+        if newTagNames:
+            for newName in newTagNames:
+                newTag, _ = Tag.objects.get_or_create(name=newName, tagType=tagType)
+                self.tags.add(newTag)
+
+        # delete any tags that are now orphaned as a result of this operation
+        for tag in oldTags:
+            if tag.song_set.count() == 0:
+                tag.delete()
