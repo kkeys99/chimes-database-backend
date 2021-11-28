72d6d64cdf0df6167a524223afd99b20691c0e97
Kevin Cook <kkeys99@yahoo.com>
2021-11-28 16:35:17 -0500

    Change import order

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,4 +1,4 @@
+from api.models.songs import *
+from api.models.performers import *
 from api.models.concerts import *
 from api.models.performances import *
-from api.models.performers import *
-from api.models.songs import *
